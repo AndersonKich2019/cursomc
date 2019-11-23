7587b9665f6535a54ba3c700242dcb87795d79bc
Anderson Kich <kich24@hotmail.com>
2019-11-23 14:43:15 -0300

    Criando operacao de instanciacao

--- src/main/java/com/andersonkich/cursomc/CursomcApplication.java
+++ src/main/java/com/andersonkich/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.andersonkich.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andersonkich.cursomc.domain.Categoria;
+import com.andersonkich.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {//CommandLineRunner usado para instanciar obetos ao iniciar a aplicação
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {//Metodo do CommandLineRunner
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		
+		repository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
