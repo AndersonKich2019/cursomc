6624db90b4f176d057162e2177519155631c5d19
Anderson Kich <kich24@hotmail.com>
2019-11-23 10:15:31 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/andersonkich/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,26 @@
+package com.andersonkich.cursomc.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andersonkich.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> list =  Arrays.asList(cat1,cat2);
+		return list;
+	}
+	
+}
