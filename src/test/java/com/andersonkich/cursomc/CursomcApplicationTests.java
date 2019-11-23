2987afffc72485b1fc5237c6cc9b9b27a906ff5c
Anderson Kich <kich24@hotmail.com>
2019-11-23 08:50:57 -0300

    Projeto criado

---
+++ src/test/java/com/andersonkich/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonkich.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
