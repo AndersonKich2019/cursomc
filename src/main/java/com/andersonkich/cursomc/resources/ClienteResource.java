a0b370e4ab15e1c7a0bc2777d668c191180f7214
Anderson Kich <kich24@hotmail.com>
2019-11-26 23:04:20 -0300

    PUT, DELETE e GET para Cliente:

--- src/main/java/com/andersonkich/cursomc/resources/ClienteResource.java
+++ src/main/java/com/andersonkich/cursomc/resources/ClienteResource.java
@@ -1,15 +1,27 @@
 package com.andersonkich.cursomc.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.andersonkich.cursomc.domain.Categoria;
 import com.andersonkich.cursomc.domain.Cliente;
+import com.andersonkich.cursomc.domain.Cliente;
+import com.andersonkich.cursomc.dto.CategoriaDto;
+import com.andersonkich.cursomc.dto.ClienteDto;
 import com.andersonkich.cursomc.services.ClienteService;
 
 @Controller
@@ -26,10 +38,35 @@ public class ClienteResource {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<Cliente>> findAll(){
+	public ResponseEntity<List<ClienteDto>> findAll(){
 		List<Cliente> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<ClienteDto> listDto = list.stream().map(obj -> new ClienteDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> update(@Valid @RequestBody ClienteDto objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDto(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();//noContent() retorna vazio
+	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<ClienteDto>> findPage(//Valores padrão caso não seja informado
+			@RequestParam(value = "page", defaultValue = "0")Integer page, 
+			@RequestParam(value = "linePerPage", defaultValue = "24")Integer linePerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction){
+		Page<Cliente> list = service.findPage(page, linePerPage, orderBy, direction);
+		Page<ClienteDto> listDto = list.map(obj -> new ClienteDto(obj));//Não precisa ser stream o Page faz a conersão automatica
+		return ResponseEntity.ok().body(listDto);
+		
+	}
 }
--- src/main/java/com/andersonkich/cursomc/services/ClienteService.java
+++ src/main/java/com/andersonkich/cursomc/services/ClienteService.java
@@ -4,10 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andersonkich.cursomc.domain.Cliente;
+import com.andersonkich.cursomc.domain.Cliente;
+import com.andersonkich.cursomc.dto.ClienteDto;
 import com.andersonkich.cursomc.repositories.ClienteRepository;
+import com.andersonkich.cursomc.services.exceptions.DataIntegrityException;
 import com.andersonkich.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -26,5 +33,49 @@ public class ClienteService {
 		return list;
 	}
 	
+	//public Cliente insert(Cliente obj) {//Post
+	//	obj.setId(null);
+	//	return repository.save(obj);
+	//}
+	
+	public Cliente update(Cliente obj) {//Put
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);//O save tambem serve para atualizar
+		
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque a entidades relacionadas");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linePerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);//Sobrecarga de metodo
+		
+		/*Integer page        = Informar qual a pagina eu quero acessar(a contagem de paginas comeca com zero)
+		 *Integer linePerPage = Informar quantas linhas por pagina
+		 *String orderBy      = Informar por qual campo eu quero ordenar (exemplo: nome, id, etc...)
+		 *String direction    = Informar qual direcao eu quero (ASC ou DESC)
+		 *PageRequest pageRequest = Objeto que prepara a consulta da pagina 
+		 */
+	}
+	
+	public Cliente fromDto(ClienteDto objDto) {
+		
+		 return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null); 
+
+	}
 	
 }
