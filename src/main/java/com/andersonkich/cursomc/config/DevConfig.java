fc1018ed9d1e0066b75ffea405de0f8e263a5e6b
Anderson Kich <kich24@hotmail.com>
2019-12-03 08:47:59 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/andersonkich/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.andersonkich.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.andersonkich.cursomc.services.DBservice;
+
+@Configuration//Para ser um arquivo de configuçao
+@Profile("dev")//indica meu profile
+public class DevConfig {
+
+	@Autowired
+	DBservice dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")//Acessa application-dev.properties
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!strategy.equals("create")) {
+			return false;
+		}
+		
+		dbService.instantiateDatabase();
+		return true;
+	}
+}
